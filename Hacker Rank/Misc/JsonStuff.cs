2611a59564760303cc08a26c9755e49883308228
VERISK\I24755 <jpconterio@verisk.com>
2022-06-10 16:01:14 -0600

    more

---
+++ Hacker Rank/Misc/JsonStuff.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks_and_Queues.Misc
+{
+	public static class JsonStuff
+	{
+		public static void DoSomething()
+		{
+			List<List<string>> data = new List<List<string>>()
+			{
+				new List<string>() { "Tom",	"25", "Blonde" },
+				new List<string>() { "Tom", "25", "Blonde" },
+				new List<string>() { "Tom", "25", "Blonde" },
+				new List<string>() { "Tom", "25", "Blonde" },
+			};
+
+			var json = ConvertToJsonObject(data);
+		}
+
+
+		private static string ConvertToJsonObject(List<List<string>> obj)
+		{
+
+
+			return "";
+		}
+		
+	}
+
+	public class Person
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+		public string HairColor { get; set; }
+	}
+}
